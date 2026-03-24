95cd796b92fa80c50b0692fa0e9f9e76bbaed60c
Dermot Boyle <dermotb@hotmail.com>
2026-03-24 20:13:15 +0000

    Add project files.

---
+++ TextMessage/Controllers/SMSController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TextMessage.Models;
+
+namespace TextMessage.Controllers
+{
+    public class SMSController : Controller
+    {
+        // GET: SMSController/Create
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        // POST: SMSController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Send(SMSMessage message)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirm", message);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET: SMSController/Edit/5
+        public ActionResult Confirm(SMSMessage msg)
+        {
+            return View(msg);
+        }
+
+
+    }
+}
---
+++ TextMessage/Models/SMSMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TextMessage.Models
+{
+    public class SMSMessage
+    {
+        [Required(ErrorMessage ="To Number must not be blank!")]
+        [StringLength(10, MinimumLength =10, ErrorMessage ="Number must be 10 digits long")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage ="Number must consist of just 10 digits")]
+        [DisplayName("To:")]
+        public string ToNumber { get; set; }
+
+        [Required(ErrorMessage ="Message cannot be blank")]
+        [StringLength(140, ErrorMessage ="Message maximum limit is 140 characters")]
+        [DisplayName("Message Content:")]
+        public string Content { get; set; }
+    }
+}
